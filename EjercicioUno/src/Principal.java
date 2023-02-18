aecd521410a1475ca4e16cd1a594561a4055298f
Estudiante USC <estudiante@GTACAD.USC>
2023-02-18 16:04:21 -0500

    primer commit

---
+++ EjercicioUno/src/Principal.java
@@ -0,0 +1,11 @@
+
+public class Principal {
+	
+	public static void main(String[] args) {
+
+		Student std = new Student();
+		System.out.println(std.iD + "\n" + std.name + "\n" + std.status);
+		
+	}
+	
+}
